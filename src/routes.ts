de9964856758c642a4535a10ac8d7b8e895ddb2d
SOPHIA ISABELLE RODRIGUES GUMBIO <sophia.gumbio@portalsesisp.org.br>
2026-04-22 13:51:16 -0300

    feat: s8 - atualizando o arquivo Routes.ts
    Protegendo as Rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
-import {Router} from "express";
+import { Router } from "express";
 import type {Request, Response} from "express";
 import AlunoController from "./controller/AlunoController.js";
 import PlanoController from "./controller/PlanoController.js";
 import MatriculaController from "./controller/MatriculaController.js";
+import { Auth } from "./middleware/Auth.js";
 
 const router: Router = Router();
 
@@ -10,17 +11,20 @@ router.get("/api", (req: Request, res: Response) => {
     res.status(200).json({ mensagem: "Olá, seja bem-vindo ao GymPro!" });
 });
 
-router.get("/api/alunos", AlunoController.todos);
-router.post("/api/alunos", AlunoController.novo);
-router.get("/api/alunos/:idAluno", AlunoController.aluno);
+// rota de login (pública)
+router.post("/api/login", Auth.validacaoUsuario);
 
-router.get("/api/planos", PlanoController.todos);
-router.post("/api/planos", PlanoController.novo);
-router.get("/api/planos/:idPlano", PlanoController.plano);
+// rotas protegidas
+router.get("/api/alunos", Auth.verifyToken, AlunoController.todos);
+router.post("/api/alunos", Auth.verifyToken, AlunoController.novo);
+router.get("/api/alunos/:idAluno", Auth.verifyToken, AlunoController.aluno);
 
-router.get("/api/matriculas", MatriculaController.todos);
-router.post("/api/matriculas", MatriculaController.novo);
-router.get("/api/matriculas/:idMatricula", MatriculaController.matricula);
+router.get("/api/planos", Auth.verifyToken, PlanoController.todos);
+router.post("/api/planos", Auth.verifyToken, PlanoController.novo);
+router.get("/api/planos/:idPlano", Auth.verifyToken, PlanoController.plano);
 
+router.get("/api/matriculas", Auth.verifyToken, MatriculaController.todos);
+router.post("/api/matriculas", Auth.verifyToken, MatriculaController.novo);
+router.get("/api/matriculas/:idMatricula", Auth.verifyToken, MatriculaController.matricula);
 
 export {router};
