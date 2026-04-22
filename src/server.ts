3608f6013072f7b560cbcd83a9701ea8bebad505
LAÍS ZANQUETA <lais.zanqueta@portalsesisp.org.br>
2026-04-22 10:57:38 -0300

    feat: modificando os arquivos server e app
    alterando os arquivos por conta que a porta estava errada

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
-import express from "express"; 
+import express from "express";
 import cors from "cors";
 import { router } from "./routes.js";
-import 'dotenv/config'; 
 
 const server = express();
 
-server.use(cors()); 
-server.use(express.json()); 
-server.use(router); 
+server.use(express.json());
+server.use(cors());
 
-const PORT = process.env.PORT || 3333;
+server.use(router);
 
-server.listen(PORT, () => {
-    console.log(` Servidor rodando em http://localhost:${PORT}`);
-});
-
-export { server };
+export { server }
